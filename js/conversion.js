955cd26dee47ab5f6ab8125bba141306ec0ffb58
Rashadul Islam <rashadul.dev@gmail.com>
2023-07-29 02:47:25 +0600

    Practice -1 Done

--- js/conversion.js
+++ js/conversion.js
@@ -2,3 +2,10 @@ var firstName="Rashadul";
 var lastName="Islam";
 var fullName=firstName+ " "+lastName;
 console.log(fullName);
+
+// Acceptable variable name 
+
+var mobile-price=56;
+var 1bookName=45;
+var user9=33;
+var "temp"=34;
---
+++ js/practice.js
@@ -0,0 +1,7 @@
+// practice -1
+var givenTk=1000;
+var apple1Kg=300;
+var orange1Kg=400;
+var totalAmount=apple1Kg+orange1Kg;
+var tkReturn=givenTk-totalAmount;
+console.log(tkReturn);
