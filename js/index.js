e61ad7e9aadc618d07bcfc895361da64ad719cb0
Rashadul Islam <rashadul.dev@gmail.com>
2023-07-28 21:59:59 +0600

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// variable
+// vary+able=varyable
+// newmaric
+var price=11;
+var age=21;
+var tempareture =35;
+
+// string
+var person="b Rabby";
+var location="mirpur Dhaka";
+
+// Boolean
+var assignment=true;
+var marks=false;
