6f1ab53db07e3759493c77c90efbef41575c3c39
Rashadul Islam <rashadul.dev@gmail.com>
2023-07-29 03:45:01 +0600

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,20 @@
 // practice -1
-var givenTk=1000;
-var apple1Kg=300;
-var orange1Kg=400;
-var totalAmount=apple1Kg+orange1Kg;
-var tkReturn=givenTk-totalAmount;
-console.log(tkReturn);
+// var givenTk=1000;
+// var apple1Kg=300;
+// var orange1Kg=400;
+// var totalAmount=apple1Kg+orange1Kg;
+// var tkReturn=givenTk-totalAmount;
+// console.log(tkReturn);
+
+
+
+// Practice Number--2
+var mathmatics=75.25;
+var biology=65;
+var chemistry=80;
+var physics=35.45;
+var bangla=99.50;
+var totalNumber=mathmatics+biology+chemistry+physics+bangla;
+var avarageMart=totalNumber/5;
+avarageMart=avarageMart.toFixed(2);
+console.log(avarageMart);
