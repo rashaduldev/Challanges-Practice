8a422b7061dedab10d74edeb69b86b9711796b8a
Rashadul Islam <rashadul.dev@gmail.com>
2023-07-30 04:10:42 +0600

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -32,3 +32,15 @@
 // var divided=5;
 // var prs=number%divided;
 // console.log(prs);
+
+
+// 
+let x = 5;
+let y = 10;
+if (x < y) {
+ console.log("x is less than y");
+} else {
+ console.log("x is greater than or equal to y");
+}
+console.log(6 != 6);
+console.log(6 >= 6);
