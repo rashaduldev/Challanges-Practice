6f57218f64c738de62f43d224d9dcea26dabcec9
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-01 18:52:59 +0600

    Update loop.js

--- js/loop.js
+++ js/loop.js
@@ -5,17 +5,22 @@
 // }
 
 // even number
-var number=0;
-while(number<5){
-    console.log(number);
-    // number++;
-    number=number+2;
-}
+// var number=0;
+// while(number<5){
+//     console.log(number);
+//     // number++;
+//     number=number+2;
+// }
 
 // Odd number
-var number=1;
-while(number<20){
-    console.log(number);
-    // number++;
-    number=number+2;
+// var number=1;
+// while(number<20){
+//     console.log(number);
+//     // number++;
+//     number=number+2;
+// }
+
+// For loop
+for(var num=10;num<10;num++){
+    console.log('good this code is working');
 }
