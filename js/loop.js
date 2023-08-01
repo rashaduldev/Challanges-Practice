eb9ef1ee3032457b8a30cba5c23763cf7a90aad9
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-02 01:01:21 +0600

    Update loop.js

--- js/loop.js
+++ js/loop.js
@@ -38,9 +38,36 @@
 // }
 //   array and loop
 
-var numbers=[11,23,25,85,76,54,65,76,54,96,89];
+// var numbers=[11,23,25,85,76,54,65,76,54,96,89];
 
-for(var i=0;i<=numbers.length;i++){
-    number=numbers[i];
-    console.log(number);
+// for(var i=0;i<=numbers.length;i++){
+//     number=numbers[i];
+//     console.log(number);
+// }
+
+// var i = 0;
+// for (i=1; i<=5; i++){};
+// console.log(i);
+// var i = 0;
+// for (i=0; i<5; i++){
+//     console.log(i);
+// };
+
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//  if(marks[i] >=15){
+//     continue;
+//  }
+//  console.log(marks[i]);
+// };
+
+// var nme="false";
+// console.log(typeof nme);
+// var i = 5;
+// for (i=0; i < 5; i++) {
+//  console.log(i);
+// }
+var i = 5;
+for (i < 5; i++) {
+ console.log(i);
 }
