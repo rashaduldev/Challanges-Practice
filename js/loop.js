f87dcfcaad03b79e6e05eba595d603b0b8e2f049
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-03 16:02:23 +0600

    Update loop.js

--- js/loop.js
+++ js/loop.js
@@ -175,3 +175,10 @@
 // for( let i = 10; i>=1; i--){
 //     console.log(i);
 //  }
+// function print(a, b, c){
+//     return a+2;
+//     return a*b;
+//     return b*c+a;
+//    }
+//    console.log(print(1,2,3));
+
