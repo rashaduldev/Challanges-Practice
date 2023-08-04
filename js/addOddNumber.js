ae33f3374d6528e914351968eef068c04d44d9f2
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-04 22:30:42 +0600

    Update addOddNumber.js

--- js/addOddNumber.js
+++ js/addOddNumber.js
@@ -1,20 +1,36 @@
 function evenNumber(array) { 
+    sum=0;
+    // console.log(array);
     for (let i = 0; i < array.length; i++) {
         const index=i;
         const element = array[i];
-        console.log(index,element);
-        if (element%2 ==0) {
-            return true;
-        }else{
-            return false;
-        }
-        
-        
+        sum=sum+element;
+        console.log(index,element,sum);
+
+        // console.log(sum);
+        // if (element%2 ==0) {
+        //     console.log("true number");
+        // } else {
+        //     console.log("False number");
+        // }
     }
+
+    // for (let i = 0; i < array.length; i++) {
+    //     const index=i;
+    //     const element = array[i];
+    //     console.log(index,element);
+    //     // if (element%2 ==0) {
+    //     //     return true;
+    //     // }else{
+    //     //     return false;
+    //     // }
+        
+        
+    // }
     // const pushArray=newArray.push(arrayName);
     //     console.log(pushArray);
 }
 const arrayName=[11,13,12,15,18,16,17,25,28,36];
 
 const numberArray=evenNumber(arrayName);
-console.log(numberArray);
+// console.log(numberArray);
