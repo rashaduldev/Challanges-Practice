9a55f4d50324cb524f1c7d93579f577a59eb1783
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-04 19:29:51 +0600

    Update loop.js

--- js/loop.js
+++ js/loop.js
@@ -182,7 +182,7 @@
 //    }
 //    console.log(print(1,2,3));
 
-let a=50;
-if(item=>50){};
-if(false){};
-for(let i=5;i<10;i=i+2){};
+// let a=50;
+// if(item=>50){};
+// if(false){};
+// for(let i=5;i<10;i=i+2){};
