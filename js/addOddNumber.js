e1b9c5ba3d54f51bac6578e3d89580207adc9a2f
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-04 22:22:52 +0600

    Update addOddNumber.js

--- js/addOddNumber.js
+++ js/addOddNumber.js
@@ -1,16 +1,20 @@
-function evenNumber(array) {
-    
+function evenNumber(array) { 
     for (let i = 0; i < array.length; i++) {
         const index=i;
         const element = array[i];
         console.log(index,element);
-        if (element%2 ==1) {
+        if (element%2 ==0) {
             return true;
         }else{
             return false;
         }
         
+        
     }
+    // const pushArray=newArray.push(arrayName);
+    //     console.log(pushArray);
 }
 const arrayName=[11,13,12,15,18,16,17,25,28,36];
+
 const numberArray=evenNumber(arrayName);
+console.log(numberArray);
