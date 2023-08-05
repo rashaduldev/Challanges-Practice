fb92e57e202b5ee1da0c828b745bf82068f6d041
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-05 14:26:19 +0600

    Update math.js

--- js/math.js
+++ js/math.js
@@ -9,3 +9,24 @@
 // } else {
 //     console.log('Stay appart');
 // }
+
+// let result=Math.round(Math.random()*10);
+// console.log(result);
+
+// function add(a, b){
+//     return a + b;
+//    }
+//    console.log(add("adam" + "eve",'b'))
+//    console.log(add(2,4))
+
+// var a = 5; 
+// var b = 8; 
+// var c = 4; 
+// var d = 6; 
+// var highest =Math.max(a, b, c, d);
+// console.log(highest);
+// console.log(Math.round(13.17));
+// const number = -78; 
+// const answer = Math.abs(number); 
+// console.log(answer);
+
