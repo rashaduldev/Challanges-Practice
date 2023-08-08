cd1eaba80872f3f7bdfcb5416c1065fe2582c645
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-08 19:17:53 +0600

    Update problem.js

--- js/problem.js
+++ js/problem.js
@@ -1,13 +1,27 @@
-function isInteger(x) {
-    if (typeof x!=="number") {
-        return "Please provide a number";
+// function isInteger(x) {
+//     if (typeof x!=="number") {
+//         return "Please provide a number";
+//     }else{
+//         if (x%1==0) {
+//             return 'true';
+//         }else{
+//             return 'false';
+//         }
+//     }
+// }
+// const intt=isInteger(5.3);
+// console.log(intt);
+
+function isJavascriptFile(s) {
+    if(typeof s !=="string") {
+        return "please provide string";
     }else{
-        if (x%1==0) {
-            return 'true';
-        }else{
-            return 'false';
+        if (s.endsWith(".js")==true) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
-const intt=isInteger(5.3);
-console.log(intt);
+const outFile=isJavascriptFile("rabby.js");
+console.log(outFile);
