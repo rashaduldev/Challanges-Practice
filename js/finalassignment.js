6d42856760edcecf3f550fdc00530196ba45cadb
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-10 10:18:32 +0600

    Update finalassignment.js

--- js/finalassignment.js
+++ js/finalassignment.js
@@ -27,33 +27,29 @@ matchFinder(firstString, secondString);
 
 function sortMaker(arr) {
     if (!Array.isArray(arr)) {
-       return "Please Provide an array";
-   }
-   for (let i = 0; i < arr.length; i++) {
-       if (typeof arr[i] !== "number" || arr[i]<0) {
-           return "Invalid Input";
-       }
-   }
-     if (arr[0] === arr[1]) {
-         return "equal";
-       }
-       else if(arr[0]>arr[1]){
-            return arr;
-       }
-   
-    else if (arr[0] < arr[1]) {
+        return "Please Provide an array";
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== "number" || arr[i] < 0) {
+            return "Invalid Input";
+        }
+    }
+    if (arr[0] === arr[1]) {
+        return "equal";
+    } else if (arr[0] > arr[1]) {
+        return arr;
+    } else if (arr[0] < arr[1]) {
         const temp = arr[0];
         arr[0] = arr[1];
         arr[1] = temp;
-      
+
         return arr;
-     }
-     else{
-       return "Something is wrong";
-     }
-  
+    } else {
+        return "Something is wrong";
+    }
+
 }
-sortMaker([2,5]);
+sortMaker([2, 5]);
 
 
 function findAddress(addressObject) {
@@ -75,18 +71,17 @@ findAddress(objectName);
 
 
 function canPay(changeArray, totalDue) {
-    if (Array.isArray(changeArray)==false) {
+    if (Array.isArray(changeArray) == false) {
         return "Please provide an array";
-    }
-    else if(changeArray.length === 0){
+    } else if (changeArray.length === 0) {
         return "Your array is empty , please provide an array";
     }
     let total = 0;
-    for (let i = 0; i <changeArray.length; i++) {
+    for (let i = 0; i < changeArray.length; i++) {
         const element = changeArray[i];
-        total+=element;   
+        total += element;
     }
-    let result= total >= totalDue;
+    let result = total >= totalDue;
     return result;
 }
-canPay([6,5], 10);
+canPay([6, 5], 10);
