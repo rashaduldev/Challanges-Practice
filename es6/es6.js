b4088d94c78124e2bf178c5e824f7c8f18065a82
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-25 13:23:03 +0600

    Update es6.js

--- es6/es6.js
+++ es6/es6.js
@@ -2,12 +2,15 @@
 // const multi=multiply(2,3,4)
 // console.log(multi);
 
-const threeLine=`I am web developer.
-I love to code.
-I love to eat biriyani
-`
-console.log(threeLine);
+// const threeLine=`I am web developer.
+// I love to code.
+// I love to eat biriyani
+// `
+// console.log(threeLine);
 
+// const twoPeramiter=(a,b=5)=>(a+b);
+// const onePera=twoPeramiter(3);
+// console.log(onePera);
 
 
 
