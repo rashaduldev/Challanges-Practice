f5f02bc18aee9508a26d099126abab8570418176
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-25 13:15:25 +0600

    Update es6.js

--- es6/es6.js
+++ es6/es6.js
@@ -1,7 +1,12 @@
-const multiply=(a,b,c)=>(a*b*c);
-const multi=multiply(2,3,4)
-console.log(multi);
+// const multiply=(a,b,c)=>(a*b*c);
+// const multi=multiply(2,3,4)
+// console.log(multi);
 
+const threeLine=`I am web developer.
+I love to code.
+I love to eat biriyani
+`
+console.log(threeLine);
 
 
 
